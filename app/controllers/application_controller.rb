8471b9691572d27b0ce02d6c876ac740ca45d5ab
Anna Lobova <riabkova.as.87@gmail.com>
2024-07-10 10:30:00 +0300

    Some methods have been moved from ApplicationController to ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,10 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  helper_method :user_has_channels?, :last_channel
 
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
-  end
-
-  def user_has_channels?
-    current_user.channels.any?
-  end
-
-  def last_channel
-    @last_message = current_user.messages.last
-    @last_channel = @last_message&.channel
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def user_has_channels?
+    current_user.channels.any?
+  end
+
+  def last_channel
+    last_message = current_user.messages.last
+    last_message&.channel
+  end
 end
