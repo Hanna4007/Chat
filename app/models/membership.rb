e6196f150e7b00403bd30e223fb8cf9aef6f68d2
Anna Lobova <riabkova.as.87@gmail.com>
2024-07-15 23:11:22 +0300

    Add a soft socket for membership removal from a channel and adding to the list of channels where current user is not membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -7,5 +7,6 @@ class Membership < ApplicationRecord
   broadcasts_to :channel, target: 'all_memberships', partial: 'memberships/list_memberships'
   broadcasts_to :user, target: 'all_memberships_for_user', partial: 'memberships/list_memberships_for_user'
   broadcasts_to :user, target: 'all_channels_for_user', partial: 'memberships/list_channels_for_user'
-
+  after_destroy_commit -> { broadcast_action_later_to(user, action: :append, target: 'all_channels_without_user', partial: 'memberships/list_channels_without_user', locals: { channel: channel, membership: nil }) }
+  
 end
