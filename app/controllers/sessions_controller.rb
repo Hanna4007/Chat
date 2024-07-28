71921a510a670a5aef5124f3df60180035f0e908
Anna Lobova <riabkova.as.87@gmail.com>
2024-07-28 18:00:44 +0300

    Fix respond_with in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     user_params = params.require(:session)
     user = User.find_by(phone_number: user_params[:phone_number])&.authenticate(user_params[:password])
     session[:user_id] = user.id if user.present?
-    respond_with(current_user, location: root_path)
+    respond_with(user, location: root_path)
   end
 
   def destroy
